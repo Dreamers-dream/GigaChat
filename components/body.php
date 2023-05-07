efed240e4fe13f79724fe24149f6dcd711b7a078
Dreamers-dream <85782968+Dreamers-dream@users.noreply.github.com>
2023-05-07 23:02:27 +0300

    тутс

---
+++ components/body.php
@@ -0,0 +1,33 @@
+<div class="container">
+ <div class="container-chat">
+    <div class="message_icon">
+      <div class="massage_display">
+          <p>User 1</p>
+          <div class="massage_send_end">
+              <p>Привет</p>
+          </div>
+      </div>
+    </div>
+    <div class="massage_send">
+        <div class="massage_f">
+            <input class="message" type="text" placeholder="Написать сообщение...">
+            <button class="send"><img class="send-icon" src="/img/send.png" style="width: 21px"></button>
+        </div>
+
+
+    </div>
+
+ </div>
+ <div class="container-login">
+     <div class="name-login">Введите логин</div>
+     <div class="massage_f">
+         <input class="message" type="text" placeholder="Логин...">
+         <button class="send"><img class="send-icon" src="/img/send.png" style="width: 21px"></button>
+     </div>
+ </div>
+</div>
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>ГигаЧат</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/container-chat.css">
+    <link rel="stylesheet" href="css/message_send.css">
+    <link rel="stylesheet" href="css/message_icon.css">
+    <link rel="stylesheet" href="css/container-login.css">
+    <link rel="stylesheet" href="css/container.css">
+
+</head>
+ <body>
+  <?php require_once "components/header.php"?>
+  <?php require_once "components/body.php"?>
+ </body>
+</html>
+
